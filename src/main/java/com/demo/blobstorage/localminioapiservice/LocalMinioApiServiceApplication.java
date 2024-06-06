e0d9632978f1a6fc984f5f99cd41d9517497b001
dineshkp-dev <dingy007@gmail.com>
2024-06-06 13:38:20 +0800

    initial commitˆ

---
+++ src/main/java/com/demo/blobstorage/localminioapiservice/LocalMinioApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.blobstorage.localminioapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalMinioApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalMinioApiServiceApplication.class, args);
+    }
+
+}
